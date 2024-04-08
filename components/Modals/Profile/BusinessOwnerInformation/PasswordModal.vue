464a7af45d039d6294f7d7b9ec1b03c4d9f5b337
hassan mohammadzadeh <73823169+emzedam@users.noreply.github.com>
2024-04-08 14:55:44 +0330

    latest commit

--- components/Modals/Profile/BusinessOwnerInformation/PasswordModal.vue
+++ components/Modals/Profile/BusinessOwnerInformation/PasswordModal.vue
@@ -45,6 +45,7 @@
                   </div>
                   <input
                     type="password"
+                    v-model="passwordData.password"
                     class="items-center block w-full pr-10 border-gray-300 rounded-md focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
                     placeholder="رمز عبور"
                   />
@@ -91,7 +92,9 @@
                   </div>
                   <input
                     type="password"
-                    class="items-center block w-full pr-10 border-gray-300 rounded-md focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
+                    :value="passwordData.newPassword"
+                    @input="(e) => set_new_password_value(e)"
+                    class="items-center block w-full focus:border-cyan-500 focus:ring-cyan-500 pr-10 border-gray-300 rounded-md focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
                     placeholder="رمز عبور"
                   />
                   <div
@@ -112,12 +115,18 @@
               </div>
             </div>
 
-            <ul class="p-3 mt-4 flex flex-col bg-gray-50 rounded-md">
-              <li class="color-500 text-caption">شامل عدد</li>
-              <li class="color-500 text-caption">حداقل ۸ حرف</li>
-              <li class="color-500 text-caption">شامل علامت (!@#$%&amp;*^)</li>
-              <li class="color-500 text-caption">شامل یک حرف بزرگ و کوچک</li>
-            </ul>
+            <ul
+            v-if="password_validator_results.length != 0"
+            class="p-3 mt-4 list-decimal flex flex-col bg-gray-50 rounded-md pr-6"
+          >
+            <li
+              v-for="(result, index) in password_validator_results"
+              :key="index"
+              class="color-500 text-caption"
+            >
+              {{ result.message }}
+            </li>
+          </ul>
 
             <div class="relative col-span-4 sm:col-span-2 mt-6">
               <label
@@ -136,7 +145,25 @@
                   </div>
                   <input
                     type="password"
-                    class="items-center block w-full pr-10 border-gray-300 rounded-md focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
+                    :class="{
+                      'focus:border-cyan-500 focus:ring-cyan-500':
+                        passwordData.repeatPassword == passwordData.newPassword &&
+                        passwordData.repeatPassword != '',
+                      'focus:border-red-500 focus:ring-red-500':
+                        passwordData.repeatPassword != passwordData.newPassword ||
+                        passwordData.repeatPassword == '',
+                      'bg-gray-100':
+                        password_validator_results.length != 0 ||
+                        passwordData.newPassword == '',
+                    }"
+                    v-model="passwordData.repeatPassword"
+                    :disabled="
+                      password_validator_results.length != 0 &&
+                      passwordData.newPassword == ''
+                        ? true
+                        : false
+                    "
+                    class="items-center block w-full pr-10 border-gray-300 rounded-md  sm:text-sm"
                     placeholder="رمز عبور"
                   />
                   <div
@@ -171,9 +198,95 @@
 </template>
 
 <script setup>
-const props = defineProps(["activeModal"]);
+import passwordValidator from "password-validator";
+import { useSellersStore } from "~/store/sellersStore";
+import { useToast } from "vue-toastification";
 
+const toast = useToast();
+const sellerStore = useSellersStore();
+const props = defineProps(["activeModal"]);
 const emit = defineEmits(["close"]);
+const sendRequestLoading = ref(false);
+const passwordValidatorSchema = new passwordValidator();
+const password_validator_results = ref([]);
+const passwordData = reactive({
+  password: "",
+  newPassword: "",
+  repeatPassword: "",
+});
+passwordValidatorSchema
+  .is()
+  .min(8, "رمز عبور شما باید حداقل ۸ حرف باشد.") // Minimum length 8
+  .is()
+  .max(100, "حداکثر تعداد کاراکتر مجاز 30 کاراکتر میباشد") // Maximum length 100
+  .has()
+  .uppercase(1, "حداقل باید از 1 حرف از حروف بزرگ لاتین استفاده کنید") // Must have uppercase letters
+  .has()
+  .lowercase(1, "حداقل باید از 1 حرف از حروف کوچک لاتین استفاده کنید") // Must have lowercase letters
+  .has()
+  .digits(2, "حداقل باید 2 کاراکتر از نوع عدد استفاده کنید") // Must have at least 2 digits
+  .has()
+  .not()
+  .spaces(0, "نباید فاصله خالی بین کاراکتر ها وجود اشته باشد") // Should not have spaces
+  .is()
+  .not()
+  .oneOf(
+    ["Passw0rd", "Password123"],
+    "گذرواژه وارد کرده باید گذرواژه ای به غیر از این ها باشد (Passw0rd , Password123)"
+  );
+const set_new_password_value = (e) => {
+  passwordData.newPassword = e ? e.target.value : "";
+
+  const validator_result = passwordValidatorSchema.validate(passwordData.newPassword, {
+    details: true,
+  });
+  password_validator_results.value = validator_result;
+};
+
+set_new_password_value();
+
+const change_password = async () => {
+  if (is_validate_password()) {
+    sendRequestLoading.value = true;
+    const result = await sellerStore.do_change_user_password(passwordData);
+    if (result.statusCode == 200) {
+      sendRequestLoading.value = false;
+      toast.success(result.message);
+
+      passwordData.password = "";
+      passwordData.newPassword = "";
+      passwordData.repeatPassword = "";
+
+      reset_modals();
+    } else {
+      sendRequestLoading.value = false;
+      toast.error(result.message);
+    }
+  }
+};
+
+const is_validate_password = () => {
+  if (passwordData.password == "") {
+    toast.error("رمز عبور فعلی خود را خالی نگدارید");
+    return false;
+  } else if (passwordData.newPassword == "") {
+    toast.error("رمز عبور جدید خود را وارد کنید");
+    return false;
+  } else if (passwordData.repeatPassword == "") {
+    toast.error("تکرار رمز عبور جدید الزامی است");
+    return false;
+  } else if (password_validator_results.value.length != 0) {
+    toast.error("لطفا تمامی معیار های تعیین شده برای گذرواژه را رعایت کنید");
+    return false;
+  } else if (passwordData.newPassword != passwordData.repeatPassword) {
+    toast.error("گذرواژه جدید با تکرار گذرواژه یکسان نیست");
+    return false;
+  } else {
+    return true;
+  }
+};
+
+
 
 const close_modal = () => {
   emit("close");
